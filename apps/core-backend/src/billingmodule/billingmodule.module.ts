d638e91dc128b9c09020a57d4dd812f943f41450
deepansh <mauryadeepansh40@gmail.com>
2025-08-20 14:43:28 +0530

    chore: completed the whole billing module by completing the final files module and controller

--- apps/core-backend/src/billingmodule/billingmodule.module.ts
+++ apps/core-backend/src/billingmodule/billingmodule.module.ts
@@ -2,13 +2,17 @@ import { Module } from '@nestjs/common';
 import { BillingmoduleRepoToken } from './application/ports/billingmodule.repo';
 import PrismaBillingModuleRepo from './infrastructure/prisma/prisma-billingmodule.repo';
 import { StripeWebhookController } from './interface/stripe-webhook.controller';
+import { BillingmoduleService } from './application/use-cases/billingmodule.service';
+import PrismaService from 'src/infra/prisma/prisma.service';
 
 @Module({
   controllers: [StripeWebhookController],
   providers: [
+    PrismaService,
+    BillingmoduleService,
     { provide: BillingmoduleRepoToken, useClass: PrismaBillingModuleRepo },
   ],
-//   exports: [BillingmoduleService],
+  exports: [BillingmoduleService],
 })
-export class BillingmoduleModule {}
+export class BillingmoduleModule { }
 
--- apps/core-backend/src/billingmodule/interface/billingmodule.controller.ts
+++ apps/core-backend/src/billingmodule/interface/billingmodule.controller.ts
@@ -1,23 +1,41 @@
-// import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-// import { BillingmoduleService } from '../application/use-cases/billingmodule.service';
+import { Body, Controller, Get, Param, Post, Delete, Patch, UseGuards } from '@nestjs/common';
+import { BillingmoduleService } from '../application/use-cases/billingmodule.service';
+import { PlanKey, BillingCycle } from '../application/ports/billingmodule.repo';
+import { JwtAuthGuard } from 'src/authmodule/infrastructure/guards/jwt-auth.guard';
 
-// @Controller('billingmodule')
-// export class BillingmoduleController {
-//   constructor(private readonly svc: BillingmoduleService) {}
+@UseGuards(JwtAuthGuard)   
+@Controller('billing')
+export class BillingmoduleController {
+    constructor(private readonly svc: BillingmoduleService) { }
 
-//   @Get()
-//   async list() {
-//     return this.svc.list();
-//   }
+    @Get(':workspaceId')
+    async getSubscription(@Param('workspaceId') workspaceId: string) {
+        return this.svc.getSubscription(workspaceId);
+    }
 
-//   @Post()
-//   async create(@Body() dto: any) {
-//     return this.svc.create(dto);
-//   }
+    @Post(':workspaceId/start-checkout')
+    async startCheckout(
+        @Param('workspaceId') workspaceId: string,
+        @Body() body: { planKey: PlanKey; cycle: BillingCycle },
+    ) {
+        return this.svc.startCheckout(workspaceId, body.planKey, body.cycle);
+    }
 
-//   @Get(':id')
-//   async get(@Param('id') id: string) {
-//     return this.svc.get(id);
-//   }
-// }
+    @Patch(':workspaceId/change-plan')
+    async changePlan(
+        @Param('workspaceId') workspaceId: string,
+        @Body() body: { planKey: PlanKey; cycle: BillingCycle },
+    ) {
+        return this.svc.changePlan(workspaceId, body.planKey, body.cycle);
+    }
 
+    @Delete(':workspaceId/:cancelParam/cancel')
+    async cancelSubscription(@Param('workspaceId') workspaceId: string, @Param('cancelParam') cancel: boolean) {
+        return this.svc.cancelSubscription(workspaceId, cancel);
+    }
+
+    @Post(':workspaceId/resume')
+    async resumeSubscription(@Param('workspaceId') workspaceId: string) {
+        return this.svc.resumeSubscription(workspaceId);
+    }
+}
